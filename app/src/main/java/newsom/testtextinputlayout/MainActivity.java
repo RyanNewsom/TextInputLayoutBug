2d0d1cc6094d455cd9bd8cd9a961a1add995841c
Ryan Newsom <Ryan.J.Newsom@kp.org>
2017-01-12 09:58:13 -0700

    init

---
+++ app/src/main/java/newsom/testtextinputlayout/MainActivity.java
@@ -0,0 +1,27 @@
+package newsom.testtextinputlayout;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mButton;
+    private TextInputLayout mTextInputLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextInputLayout = (TextInputLayout) findViewById(R.id.username_wrapper);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTextInputLayout.setHintTextAppearance(R.color.error_red);
+            }
+        });
+    }
+}
